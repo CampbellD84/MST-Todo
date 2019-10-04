6d57c7394ec90925216f1250e7e6e6e8937b669d
Douglas Campbell <dacampbelltbone@gmail.com>
2019-10-04 05:19:37 -0400

    Todo app to learn Mobx State Tree

--- src/index.js
+++ src/index.js
@@ -1,12 +1,79 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {render} from 'react-dom';
+import {types} from 'mobx-state-tree';
+import {observer} from 'mobx-react';
+import {values} from 'mobx';
+
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let id = 1;
+const randomId = () => ++id;
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Todo = types.model({
+    id: types.identifierNumber,
+    name: types.optional(types.string, ""),
+    done: types.optional(types.boolean, false)
+}).actions(self => {
+    function setName(newName) {
+        self.name = newName;
+    }
+
+    function toggle() {
+        self.done = !self.done;
+    }
+
+    return { setName, toggle }
+});
+
+const User = types.model({
+    name: types.optional(types.string, "")
+});
+
+const RootStore = types.model({
+    users: types.map(User),
+    todos: types.optional(types.map(Todo), {})
+}).actions(self => {
+    function addTodo(id, name) {
+        self.todos.set(id, Todo.create({id, name}));
+    }
+
+    return {addTodo};
+});
+
+const store = RootStore.create({
+    users: {},
+    todos: {
+        "1": {
+            id: id,
+            name: "Eat an entire cake",
+            done: true
+        }
+    }
+});
+
+const App = observer(props => (
+    <div>
+        <button onClick={e => props.store.addTodo(randomId(), "New Task")}>
+            Add Task
+        </button>
+        {values(props.store.todos).map(todo => (
+            <div>
+                <input
+                    type="checkbox"
+                    checked={todo.done}
+                    onChange={e => todo.toggle()}
+                />
+                <input
+                    type="text"
+                    value={todo.name}
+                    onChange={e => todo.setName(e.target.value)}
+                />
+            </div>
+        ))}
+    </div>
+));
+
+
+render(<App store={store}/>, document.getElementById('root'));
+
+
